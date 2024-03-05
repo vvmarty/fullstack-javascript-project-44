1617710d155968150f190851d8f5e129dd53bbd9
vvmarty <vvmarty@mail.ru>
2024-03-05 21:09:32 +0300

    step 4: fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { questionName } from "../src/cli.js";
+import { questionName } from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 questionName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 export const questionName = () => {
-  const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
